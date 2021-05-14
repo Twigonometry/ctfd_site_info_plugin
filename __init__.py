56ef7d53b1d78aa112d5a959b34b1bd4a2501da9
Mac Goodwin <mgoodwin4@sheffield.ac.uk>
2021-05-14 18:39:46 +0100

    ?Fix??

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import request, send_from_directory
 from CTFd.plugins import register_plugin_assets_directory
 def load(app):
-    base_path='/plugins/ctfd_site_info_plugin/assets/'
+    base_path='plugins/ctfd_site_info_plugin/assets/'
     register_plugin_assets_directory(app,base_path=base_path)
     @app.route('/robots.txt')
-    def robots():
-        return send_from_directory(base_path.strip("/"),"robots.txt")
+    def send_robots():
+        return send_from_directory(base_path.strip("/"),request.path[1:])
